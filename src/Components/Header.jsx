0e758125a78aea57ce85c2c26644815bbf0de044
ShailyDoshi <shailydoshi03@gmail.com>
2024-08-01 22:38:05 +0530

    bg change again

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   const toggleMenu = () => setMenuOpend(!menuOpened);
   return (
     <div>
-      <header className="fixed top-0 left-0 m-auto  w-full bg-transparent ring-1 ring-slate-900/5 z-10">
+      <header className="fixed top-0 left-0 m-auto  w-full bg-white ring-1 ring-slate-900/5 z-10">
         <div className="px-4 flexBetween py-3 max-xs:px-2 ">
           <div>
             <Link>
